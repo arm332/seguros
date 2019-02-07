39cc955e0640838aa579c312c54afdc49e1bf7e5
André Moreira <arm332@gmail.com>
2019-02-06 22:17:57 -0200

    Sign-in with Google, get account and credential to user Drive API

--- app/src/main/java/com/arm332/seguros2/SyncActivity.java
+++ app/src/main/java/com/arm332/seguros2/SyncActivity.java
@@ -3,29 +3,102 @@ package com.arm332.seguros2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Collections;
 
 public class SyncActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String EXTRA_RESULT = "com.arm332.seguros2.EXTRA_RESULT";
-
+    // private static final String[] SCOPES = { SheetsScopes.SPREADSHEETS_READONLY };
     private static final String TAG = "SyncActivity";
+    private static final Integer RC_SIGN_IN = 1;
+    private GoogleSignInClient mGoogleSignInClient = null;
+    private SignInButton mSignInButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
 
-        Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(this);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(DriveScopes.DRIVE_READONLY))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        mSignInButton = findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(this);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        getGoogleAccountCredential(account);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent();
-        intent.putExtra(EXTRA_RESULT, "RESULT");
+//        Intent intent = new Intent();
+//        intent.putExtra(EXTRA_RESULT, "RESULT");
+//        setResult(RESULT_OK, intent);
+//        finish();
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+                break;
+        }
+    }
 
-        setResult(RESULT_OK, intent);
-        finish();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // Signed in successfully, show authenticated UI.
+                getGoogleAccountCredential(account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                //updateUI(null);
+            }
+        }
+    }
+
+    private void getGoogleAccountCredential(GoogleSignInAccount account) {
+        if (account != null) {
+            GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                    this.getApplicationContext(),
+                    Collections.singleton(DriveScopes.DRIVE_READONLY));
+            credential.setSelectedAccount(account.getAccount());
+            // new SyncTask(this, credential).execute();
+        }
     }
 }
