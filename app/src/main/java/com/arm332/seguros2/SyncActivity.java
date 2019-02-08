386fd228fa0b2587aa416c747f722fd2b70ac7e6
André Moreira <arm332@gmail.com>
2019-02-08 12:08:56 -0200

    Manage spreadsheet id on preferences, save spreadsheet range values to file and finish activity

--- app/src/main/java/com/arm332/seguros2/SyncActivity.java
+++ app/src/main/java/com/arm332/seguros2/SyncActivity.java
@@ -1,18 +1,18 @@
 package com.arm332.seguros2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.EditText;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Scope;
@@ -23,17 +23,23 @@ import com.google.api.services.drive.DriveScopes;
 import java.util.Collections;
 
 public class SyncActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String EXTRA_RESULT = "com.arm332.seguros2.EXTRA_RESULT";
-    // private static final String[] SCOPES = { SheetsScopes.SPREADSHEETS_READONLY };
-    private static final String TAG = "SyncActivity";
-    private static final Integer RC_SIGN_IN = 1;
+    public static final String TAG = "SyncActivity";
+    public static final Integer RC_SIGN_IN = 1;
+    private SharedPreferences mPrefs;
+    private String mSpreadsheetId;
+    private EditText mEditText;
     private GoogleSignInClient mGoogleSignInClient = null;
-    private SignInButton mSignInButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mSpreadsheetId = mPrefs.getString("spreadsheet_id", null);
+
+        mEditText = findViewById(R.id.editText);
+        mEditText.setText(mSpreadsheetId);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -46,23 +52,25 @@ public class SyncActivity extends AppCompatActivity implements View.OnClickListe
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         // Set the dimensions of the sign-in button.
-        mSignInButton = findViewById(R.id.sign_in_button);
-        mSignInButton.setOnClickListener(this);
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
 
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        getGoogleAccountCredential(account);
+        //
+        updateUI(account);
     }
 
     @Override
     public void onClick(View v) {
-//        Intent intent = new Intent();
-//        intent.putExtra(EXTRA_RESULT, "RESULT");
-//        setResult(RESULT_OK, intent);
-//        finish();
         switch (v.getId()) {
             case R.id.sign_in_button:
+                mSpreadsheetId = mEditText.getText().toString().trim();
+                SharedPreferences.Editor editor = mPrefs.edit();
+                editor.putString("spreadsheet_id", mSpreadsheetId);
+                editor.apply();
+
                 Intent signInIntent = mGoogleSignInClient.getSignInIntent();
                 startActivityForResult(signInIntent, RC_SIGN_IN);
                 break;
@@ -82,23 +90,24 @@ public class SyncActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 // Signed in successfully, show authenticated UI.
-                getGoogleAccountCredential(account);
+                updateUI(account);
             } catch (ApiException e) {
                 // The ApiException status code indicates the detailed failure reason.
                 // Please refer to the GoogleSignInStatusCodes class reference for more information.
                 Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-                //updateUI(null);
+                updateUI(null);
             }
         }
     }
 
-    private void getGoogleAccountCredential(GoogleSignInAccount account) {
+    public void updateUI(GoogleSignInAccount account) {
         if (account != null) {
             GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
-                    this.getApplicationContext(),
+                    getApplicationContext(),
                     Collections.singleton(DriveScopes.DRIVE_READONLY));
             credential.setSelectedAccount(account.getAccount());
-            new SyncTask(this, credential).execute();
+
+            new SyncTask(this, credential).execute(mSpreadsheetId);
         }
     }
 }
--- app/src/main/java/com/arm332/seguros2/SyncTask.java
+++ app/src/main/java/com/arm332/seguros2/SyncTask.java
@@ -1,57 +1,94 @@
 package com.arm332.seguros2;
 
+import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.SheetProperties;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SyncTask extends AsyncTask<Void, Void, String> {
-    private static final String APP_NAME = "Seguros";
+public class SyncTask extends AsyncTask<String, Void, String> {
+    private static final String APPLICATION_NAME = "Seguros";
     private WeakReference<SyncActivity> mActivity;
     private GoogleAccountCredential mCredential;
 
     public SyncTask(SyncActivity activity, GoogleAccountCredential credential) {
-        this.mActivity = new WeakReference<>(activity);
-        this.mCredential = credential;
+        mActivity = new WeakReference<>(activity);
+        mCredential = credential;
     }
 
     @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-    }
+    protected String doInBackground(String... params) {
+        String spreadsheetId = params[0];
 
-    @Override
-    protected String doInBackground(Void... voids) {
-        // HttpTransport transport = AndroidHttp.newCompatibleTransport(); // Deprecated
         // HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport(); // JKS not found
+        // HttpTransport transport = AndroidHttp.newCompatibleTransport(); // Deprecated
         HttpTransport httpTransport = new NetHttpTransport();
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        Drive driveService = new Drive.Builder(httpTransport, jsonFactory, mCredential)
-                .setApplicationName(APP_NAME)
+        Sheets service = new Sheets.Builder(httpTransport, jsonFactory, mCredential)
+                .setApplicationName(APPLICATION_NAME)
                 .build();
 
         try {
-            FileList result = driveService.files().list()
-                    .setQ("mimeType='application/vnd.google-apps.spreadsheet'")
-                    .setSpaces("drive")
-                    .execute();
+            Spreadsheet spreadsheet = service.spreadsheets().get(spreadsheetId).execute();
 
-            List<File> files = result.getFiles();
+//                String jsonString = spreadsheet.toString();
+//                Log.d(TAG, "jsonString: " + jsonString);
+//                jsonString: {"properties":{"autoRecalc":"ON_CHANGE","defaultFormat":{"backgroundColor":{"blue":1.0,"green":1.0,"red":1.0},"padding":{"left":3,"right":3},"textFormat":{"bold":false,"fontFamily":"Arial","fontSize":10,"foregroundColor":{},"italic":false,"strikethrough":false,"underline":false},"verticalAlignment":"BOTTOM","wrapStrategy":"OVERFLOW_CELL"},"locale":"pt_BR","timeZone":"Etc/GMT","title":"Seguros - editar somente esse aqui Adriana"},"sheets":[{"properties":{"gridProperties":{"columnCount":32,"rowCount":929},"index":0,"sheetId":2067206578,"sheetType":"GRID","title":"SEGUROS"}},{"properties":{"gridProperties":{"columnCount":26,"rowCount":1000},"index":1,"sheetId":1153567913,"sheetType":"GRID","title":"CADASTRO DOM BOSCO"}}],"spreadsheetId":"1un8-X5z1ddAt6kxN_FBb42eCLkmVw6FakfWR4a89U3g","spreadsheetUrl":"https://docs.google.com/spreadsheets/d/1un8-X5z1ddAt6kxN_FBb42eCLkmVw6FakfWR4a89U3g/edit"}
 
-            for (File file : files) {
-                System.out.printf("Found file: %s (%s)\n", file.getName(), file.getId());
+            SpreadsheetProperties spreadsheetProperties = spreadsheet.getProperties();
+//                String spreadsheetTitle = spreadsheetProperties .getTitle();
+//                Log.d(TAG, "spreadsheetTitle: " + spreadsheetTitle);
+
+            List<Sheet> sheets = spreadsheet.getSheets();
+
+            if (sheets.size() != 0) {
+                SheetProperties sheetProperties = sheets.get(0).getProperties();
+                String sheetTitle = sheetProperties.getTitle();
+//                    Log.d(TAG, "sheetTitle: " + sheetTitle);
+//                    Log.d(TAG, "columnCount: " + sheetProperties.getGridProperties().getColumnCount()); // 32
+//                    Log.d(TAG, "frozenRowCount: " + sheetProperties.getGridProperties().getFrozenRowCount()); // null
+//                    Log.d(TAG, "rowCount: " + sheetProperties.getGridProperties().getRowCount()); // 929
+
+                ValueRange valueRange = service.spreadsheets().values()
+                        .get(spreadsheetId, sheetTitle)
+                        .execute();
+
+//                String jsonString = valueRange.toString();
+//                Log.d("FOO", "jsonString: " + jsonString);
+
+//                List<List<Object>> values = valueRange.getValues();
+//                Log.d(TAG, "values.size: " + values.size()); // 308
+
+//                for (List<Object> row : values) {
+//                    for (Object col : row) {
+//                        String val = col.toString();
+//                    }
+//                }
+
+//                // Convert List<Object> to List<String> <https://stackoverflow.com/a/4581429>
+//                List<String> strings = new ArrayList<>(valueRange.size());
+//
+//                for (Object object : list) {
+//                    strings.add(String.valueOf(object));
+//                }
+
+                return valueRange.toString();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -59,13 +96,15 @@ public class SyncTask extends AsyncTask<Void, Void, String> {
     }
 
     @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
 
         SyncActivity activity = mActivity.get();
 
-        if (activity != null) {
-            // TODO
+        if (activity != null && !activity.isFinishing()) {
+            Utils.str2file(activity, result);
+            activity.setResult(Activity.RESULT_OK);
+            activity.finish();
         }
     }
 }
