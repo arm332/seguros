055a6760d0693d89534fe367a65ac9263500190e
André Moreira <arm332@gmail.com>
2019-02-09 14:24:53 -0200

    Fix SearchView style on Android 4.1

--- app/src/main/java/com/arm332/seguros2/ListActivity.java
+++ app/src/main/java/com/arm332/seguros2/ListActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SearchView;
 
 public class ListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
 //    private static final String TAG = "ListActivity";
