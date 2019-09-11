1211ee9292d5869a02b7706c3040876521173bda
arm332 <arm332@gmail.com>
2019-09-11 10:50:40 -0300

    Store password preference hash key and show spreadsheet name

--- app/src/main/java/com/arm332/seguros2/SettingsActivity.java
+++ app/src/main/java/com/arm332/seguros2/SettingsActivity.java
@@ -1,11 +1,18 @@
 package com.arm332.seguros2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+// https://stackoverflow.com/q/7275015
+// https://stackoverflow.com/q/18509369
+
 public class SettingsActivity extends AppCompatActivity {
+    public static final String PASSWORD_KEY = "password_key";
+    public static final String SPREADSHEET_KEY = "spreadsheet_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +25,44 @@ public class SettingsActivity extends AppCompatActivity {
                 .commit();
     }
 
-    public static class MySettingsFragment extends PreferenceFragmentCompat {
+    public static class MySettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+        private SharedPreferences mPref;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.settings, rootKey);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            mPref = getPreferenceManager().getSharedPreferences();
+            mPref.registerOnSharedPreferenceChangeListener(this);
+
+            updateSummary(SPREADSHEET_KEY);
+        }
+
+        @Override
+        public void onPause() {
+            mPref.unregisterOnSharedPreferenceChangeListener(this);
+
+            super.onPause();
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (SPREADSHEET_KEY.equals(key)) {
+                updateSummary(key);
+            }
+        }
+
+        private void updateSummary(String key) {
+            Preference p = findPreference(key);
+
+            if (p != null) {
+                p.setSummary(mPref.getString(key, ""));
+            }
         }
     }
 }
